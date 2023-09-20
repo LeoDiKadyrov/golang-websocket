7bfbc02bb98009e3ee541f50c38df48722fd2cf3
Arystan <din02winchester@gmail.com>
2023-09-20 13:24:45 +0300

    fixed validation and password hashing

--- server/registration/security.go
+++ server/registration/security.go
@@ -7,19 +7,16 @@ import (
 )
 
 func HashAndSaltPassword(password string) (string, error) {
-    salt := make([]byte, 16)
-    _, err := rand.Read(salt)
-    if err != nil {
-        return "", err
-    }
-
+	salt := make([]byte, 16)
+	_, err := rand.Read(salt)
+	if err != nil {
+		return "", err
+	}
 	combined := append([]byte(password), salt...)
 	hashedPassword, err := bcrypt.GenerateFromPassword(combined, bcrypt.DefaultCost)
-    if err != nil {
-        return "", err
-    }
+	if err != nil {
+		return "", err
+	}
 
-    // Store the salt and hashed password in the database
-    saltAndPassword := append(salt, hashedPassword...)
-    return string(saltAndPassword), nil
+	return string(hashedPassword), nil
 }
