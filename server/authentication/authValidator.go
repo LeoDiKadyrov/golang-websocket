db91a951641dfb0040277b6f4878a6bfab9b7cec
Arystan <din02winchester@gmail.com>
2023-09-26 08:31:21 +0300

    Added styles to recover page

---
+++ server/authentication/authValidator.go
@@ -0,0 +1,10 @@
+package authentication
+
+import (
+	"fmt"
+)
+
+func authValidator() {
+	fmt.Println("I'm an auth validator")
+	// use security.go here not to violate DRY !!!
+}
---
+++ server/recoverPassword/recoveryPassword.go
@@ -0,0 +1,9 @@
+package recoverpassword
+
+import (
+	"fmt"
+)
+
+func recoverPassword() {
+	fmt.Println("I'm a recover password")
+}
