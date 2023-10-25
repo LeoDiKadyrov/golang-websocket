9b7a575d0f9b3e514233f6c0921ae87dc03dc388
Arystan <a_r_s_t_a_n@mail.ru>
2023-10-25 20:26:06 +0700

    moved all auth into one file

--- server/authentication/authentication.go
+++ server/authentication/authentication.go
@@ -2,15 +2,42 @@ package authentication
 
 import (
 	"database/sql"
+	"encoding/json"
+	"net/http"
 	"errors"
 	"log"
 
-	postgresdb "websocket_1/server/database"
 	"websocket_1/server/models"
+	"websocket_1/server/validation"
 	"golang.org/x/crypto/bcrypt"
+	postgresdb "websocket_1/server/database"
 )
 
-func AuthenticateUser(username, password string) error {
+func AuthHandler(w http.ResponseWriter, r *http.Request) {
+	var request models.User
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	username := request.Username
+	password := request.PasswordHash
+
+	if !validation.IsValidPassword(password) || !validation.IsValidUsername(username) {
+		http.Error(w, "Invalid authentication input", http.StatusBadRequest)
+		return
+	}
+
+	if err := authenticateUser(username, password); err != nil {
+		http.Error(w, "Authentication failed", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Authentication successful"))
+}
+
+func authenticateUser(username, password string) error {
 	var user models.User
 	db := postgresdb.GetInstanceDB().DB
 
